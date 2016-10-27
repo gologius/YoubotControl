7993026b082287ece2ff2b65e0041cb1dd6398d1
gologius <23093843+gologius@users.noreply.github.com>
2016-10-27 16:56:28 +0900

    update Readme

--- KUKAController.py
+++ KUKAController.py
@@ -1,35 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-------------------------------------------------------
 This script is an example of TCP client for rosbridge.
 
 Install rosbridge on the remote machine.
 $ sudo apt-get install ros-indigo-rosbridge-suite
+$ sudo apt-get install ros-indigo-youbot-driver
 
-Run the turtlesim and the rosbridge TCP server.
+Run the youbot driver and the rosbridge TCP server.
 $ roscore
 $ roslaunch rosbridge_server rosbridge_tcp.launch
-$ rosrun youbot_driv
+$ sudo su - 
+$ roslaunch youbot_driver_ros_interface youbot_driver.launch
 
-Then run this script, and you can control the turtle.
 """
 
 import socket
 import json
-
-
-
 
 def make_message(twist):
     return json.dumps(dict(op='publish',
                            topic='/cmd_vel',
                            msg=twist))
 
-
 def make_twist(x_lin, y_lin, z_lin, x_ang, y_ang, z_ang):
     return dict(linear=dict(x=x_lin, y=y_lin, z=z_lin),
                 angular=dict(x=x_ang, y=y_ang, z=z_ang))
-
 
 if __name__ == '__main__':
     host = '192.168.100.40'
@@ -39,14 +34,6 @@ if __name__ == '__main__':
 
     sock = socket.socket()
     sock.connect((host, port))
-
-    """
-    advertise = json.dumps(dict(op='advertise', 
-                                topic='/turtle1/cmd_vel',
-                                type='geometry_msgs/Twist'))
-    sock.send(advertise)
-    """
-
     
     print "Use WASD keys (& Enter) to move the youbot."
     while True:
@@ -60,7 +47,7 @@ if __name__ == '__main__':
         elif key == 's':
             x_lin = -lin_vel
         elif key == 'd':
-            z_ang = -ang_
+            z_ang = -ang_vel
         elif key == 'q':
             break;                
         else:
@@ -70,7 +57,7 @@ if __name__ == '__main__':
         message = make_message(twist)
         sock.send(message)
     
-    #stop
+    #stop youbot
     twist = make_twist(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
     message = make_message(twist)
     sock.send(message)
